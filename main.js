89974f169c9c25c7aa196718690ae16050bee098
tsankovelichkov <80650325+tsankovelichkov@users.noreply.github.com>
2021-11-08 14:09:01 +0200

    creating login routes and login template

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-function onInit(){
+function onInit(){ 
     let navigationTemplate=Handlebars.compile(document.getElementById('navigation-template').innerHTML)
     Handlebars.registerPartial("navigation-template",navigationTemplate)
-    router('home')
+    navigate('/home')
+
+    document.querySelector('.navi').addEventListener('click',e=>{
+        e.preventDefault()
+        if(e.target.classList.contains('nav-link')){
+            let url = new URL(e.target.href)
+
+            switch (url.pathname) {
+                case '/login':
+                    navigate('/login')
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    })
 }
+
+
+
+
 
 onInit()
--- router.js
+++ router.js
@@ -1,10 +1,15 @@
 const routes = {
-    "home":"home-guest-template"
+    "/home":"home-guest-template",
+    "/login":"login-template",
 }
 
 function router(path) {
-    history.pushState({},'',path)
     let app = document.getElementById('app')
     let template=Handlebars.compile(document.getElementById(routes[path]).innerHTML)
     app.innerHTML = template()
+}
+
+function navigate(path) {
+    history.pushState({},'',path)
+    router(path)
 }
